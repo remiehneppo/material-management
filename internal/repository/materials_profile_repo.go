701b54eb91aa921ad65af7d525a81d814dac982a
remiehneppo <bao.tran080898@gmail.com>
2025-08-11 20:14:08 +0700

    Add UpdateEstimateMaterials and UpdateRealityMaterials methods to MaterialsProfileRepository
    - Add interface methods for updating estimate and reality materials
    - Implement safe update logic by fetching full object first
    - Avoid MongoDB  operator conflicts by updating entire document

--- internal/repository/materials_profile_repo.go
+++ internal/repository/materials_profile_repo.go
@@ -13,6 +13,8 @@ type MaterialsProfileRepository interface {
 	Save(ctx context.Context, materialsProfile *types.MaterialsProfile) error
 	FindByID(ctx context.Context, id string) (*types.MaterialsProfile, error)
 	Filter(ctx context.Context, filter *types.MaterialsProfileFilter) ([]*types.MaterialsProfile, error)
+	UpdateEstimateMaterials(ctx context.Context, id string, estimateMaterials types.MaterialsForEquipment) error
+	UpdateRealityMaterials(ctx context.Context, id string, realityMaterials types.MaterialsForEquipment) error
 }
 
 type materialsProfileRepository struct {
@@ -47,4 +49,30 @@ func (r *materialsProfileRepository) Filter(ctx context.Context, filter *types.M
 		return nil, err
 	}
 	return materialsProfiles, nil
+}
+
+func (r *materialsProfileRepository) UpdateEstimateMaterials(ctx context.Context, id string, estimateMaterials types.MaterialsForEquipment) error {
+	materialsProfile, err := r.FindByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	materialsProfile.Estimate = estimateMaterials
+	err = r.database.Update(ctx, r.collection, id, materialsProfile)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *materialsProfileRepository) UpdateRealityMaterials(ctx context.Context, id string, realityMaterials types.MaterialsForEquipment) error {
+	materialsProfile, err := r.FindByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	materialsProfile.Reality = realityMaterials
+	err = r.database.Update(ctx, r.collection, id, materialsProfile)
+	if err != nil {
+		return err
+	}
+	return nil
 }
